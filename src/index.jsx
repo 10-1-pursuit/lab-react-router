b44618407332d8af51c6037924281d2fa2b28c02
Rose <i.a.rose.bk@gmail.com>
2023-08-10 11:24:33 -0400

    added routes

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-import App from "./App";
+import App from "./App.jsx";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+ReactDOM.render(
+<App />,
+ document.getElementById("root"));
